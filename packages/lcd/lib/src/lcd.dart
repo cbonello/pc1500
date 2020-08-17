5e93aaedd81ff2daff0b27cc0bcdbcdce5d42733
Christophe Bonello <cbonello@users.noreply.github.com>
2020-08-17 18:28:32 -0400

    add tests

--- packages/lcd/lib/src/lcd.dart
+++ packages/lcd/lib/src/lcd.dart
@@ -1,11 +1,14 @@
 import 'package:chip_select_decoder/chip_select_decoder.dart';
+import 'package:meta/meta.dart';
 
 typedef MemoryRead = int Function(int address);
 
 class Lcd with MemoryObserver {
-  Lcd({this.memRead});
+  Lcd({@required MemoryRead memRead})
+      : assert(memRead != null),
+        _memRead = memRead;
 
-  final MemoryRead memRead;
+  final MemoryRead _memRead;
 
   @override
   void update(MemoryAccessType type, int address, int value) {}
--- packages/lcd/test/lcd_test.dart
+++ packages/lcd/test/lcd_test.dart
@@ -1,16 +1,26 @@
+import 'dart:typed_data';
+
 import 'package:lcd/lcd.dart';
 import 'package:test/test.dart';
 
-void main() {
-  group('A group of tests', () {
-    Awesome awesome;
+final Uint8ClampedList me0 = Uint8ClampedList(64 * 1024);
 
-    setUp(() {
-      awesome = Awesome();
+void memLoad(int address, List<int> data) =>
+    me0.setRange(address, address + data.length, data);
+
+int memRead(int address) => me0[address];
+
+void main() {
+  group('Lcd', () {
+    test('should raise an exception for invalid arguments', () {
+      expect(
+        () => Lcd(memRead: null),
+        throwsA(const TypeMatcher<AssertionError>()),
+      );
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('should be intialized successfully', () {
+      expect(Lcd(memRead: memRead), isA<Lcd>());
     });
   });
 }
