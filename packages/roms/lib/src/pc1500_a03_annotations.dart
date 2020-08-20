cac72c39b92897e7792f57853bc6f72bab153cd5
Christophe Bonello <cbonello@users.noreply.github.com>
2020-08-20 14:05:28 -0400

    update annotations of PC-1500 ROM version A03

--- packages/roms/lib/src/pc1500_a03_annotations.dart
+++ packages/roms/lib/src/pc1500_a03_annotations.dart
@@ -1,154 +1,194 @@
 const String json = '''
 {
-    "7894": {
-        "comment": "String buffer pointer"
-    },
-    "7A00-7A07": {
-        "comment": "Arithmetic register"
-    },
-    "D925": {
-        "comment": "System subroutine: Combination of character"
-    },
-    "D9DC": {
-        "comment": "System subroutine: CHR\$"
-    },
-    "D9CF": {
-        "comment": "System subroutine: STR\$"
-    },
-    "D9D7": {
-        "comment": "System subroutine: VAL"
-    },
-    "D9DD": {
-        "comment": "System subroutine: RIGHT\$, MID\$, LEFT\$"
-    },
-    "EFB6": {
-        "comment": "System subroutine: Subtract"
-    },
-    "EFBA": {
-        "comment": "System subroutine: Add"
-    },
-    "F01A": {
-        "comment": "System subroutine: Multiply"
-    },
-    "F084": {
-        "comment": "System subroutine: Divide"
-    },
-    "F161": {
-        "comment": "System subroutine: LN"
-    },
-    "F165": {
-        "comment": "System subroutine: LOG"
-    },
-    "F1CB": {
-        "comment": "System subroutine: EXP"
-    },
-    "F1D4": {
-        "comment": "System subroutine: 10^n"
-    },
-    "F391": {
-        "comment": "System subroutine: COS"
-    },
-    "F39E": {
-        "comment": "System subroutine: TAN"
-    },
-    "F3A2": {
-        "comment": "System subroutine: SIN"
-    },
-    "F492": {
-        "comment": "System subroutine: ACS"
-    },
-    "F496": {
-        "comment": "System subroutine: ATN"
-    },
-    "F49A": {
-        "comment": "System subroutine: ASN"
-    },
-    "F531": {
-        "comment": "System subroutine: DEG"
-    },
-    "F564": {
-        "comment": "System subroutine: DMS"
-    },
-    "F597": {
-        "comment": "System subroutine: ABS"
-    },
-    "F59D": {
-        "comment": "System subroutine: SGN"
-    },
-    "F5BE": {
-        "comment": "System subroutine: INT"
-    },
-    "F89C": {
-        "comment": "System subroutine: Power raise"
-    },
-    "D0D2": {
-        "comment": "System subroutine: Numerical comparison"
-    },
-    "D0F9": {
-        "comment": "System subroutine: Character string comparison"
-    },
-    "D2EA": {
-        "comment": "System subroutine: Line number search"
-    },
-    "E42C": {
-        "comment": "System subroutine: KEY scan (I)"
-    },
-    "D461": {
-        "comment": "System subroutine: Variable search"
-    },
-    "E243": {
-        "comment": "System subroutine: KEY scan (II)"
-    },
-    "E33F": {
-        "comment": "System subroutine: Auto-power-off"
-    },
-    "E8CA": {
-        "comment": "System subroutine: Program display"
-    },
-    "EDEF": {
-        "comment": "System subroutine: Graphic display"
-    },
-    "ED95": {
-        "comment": "System subroutine: Hexadecimal display (2 bytes - 1 byte)"
-    },
-    "ED57": {
-        "comment": "System subroutine: One character display"
-    },
-    "ED3B": {
-        "comment": "System subroutine: 'n' character display"
-    },
-    "ED4D": {
-        "comment": "System subroutine: Cursor move after one character display"
-    },
-    "ED00": {
-        "comment": "System subroutine: Cursor move after 'n' character display"
-    },
-    "D9F3": {
-        "comment": "System subroutine: STR\$"
-    },
-    "E000": {
-        "comment": "Reset Subroutine"
-    },
-    "E004": {
-        "comment": "Disable timer"
-    },
-    "E006": {
-        "comment": "LCD off"
-    },
-    "E00A": {
-        "comment": "Wait for 192*11 CPU cycles (about 1.6 ms)"
-    },
-    "E00C": {
-        "comment": "LCD on"
-    },
-    "E010": {
-        "comment": "Wait for 1.6 ms"
-    },
-    "E012": {
-        "comment": "LCD off"
-    },
-    "E014-E01C": {
-        "comment": "Wait for 11,496 CPU cycles (about 9 ms)"
+    "memory-bank": 0,
+    "sections": {
+        "C000-FFFF": {
+            "name": "PC-1500 SYSTEM PROGRAM (ROM), 16KB",
+            "type": "rom",
+            "content": {
+                "D925": {
+                    "name": "COMBINATION_OF_CHARACTERS"
+                    "comment": "System subroutine: Combination of two character strings"
+                },
+                "D9DC": {
+                    "name": "CHR\$"
+                    "comment": "System subroutine: Convert ASCII code to character"
+                },
+                "D9CF": {
+                    "name": "STR\$"
+                    "comment": "System subroutine: Convert a numerical expression to a string"
+                },
+                "D9D7": {
+                    "name": "VAL"
+                    "comment": "System subroutine: Convert a string to a number"
+                },
+                "D9DD": {
+                    "name": "RIGHT\$_MID\$_LEFT\$"
+                    "comment": "System subroutines: Extract characters from a string"
+                },
+                "EFB6": {
+                    "name": "SUBTRACT"
+                    "comment": "System subroutine: Subtract"
+                },
+                "EFBA": {
+                    "name": "ADD"
+                    "comment": "System subroutine: Add"
+                },
+                "F01A": {
+                    "name": "MULTIPLY"
+                    "comment": "System subroutine: Multiply"
+                },
+                "F084": {
+                    "name": "DIVIDE"
+                    "comment": "System subroutine: Divide"
+                },
+                "F161": {
+                    "name": "LN"
+                    "comment": "System subroutine: Natural logarithm to base e"
+                },
+                "F165": {
+                    "name": "LOG"
+                    "comment": "System subroutine: Common logarithm to base 10"
+                },
+                "F1CB": {
+                    "name": "EXP"
+                    "comment": "System subroutine: Natural antilog (e^n)"
+                },
+                "F1D4": {
+                    "name": "10N"
+                    "comment": "System subroutine: 10 to the power of N"
+                },
+                "F391": {
+                    "name": "COS"
+                    "comment": "System subroutine: Cosine"
+                },
+                "F39E": {
+                    "name": "TAN"
+                    "comment": "System subroutine: Tangent"
+                },
+                "F3A2": {
+                    "name": "SIN"
+                    "comment": "System subroutine: Sine"
+                },
+                "F492": {
+                    "name": "ACS",
+                    "comment": "System subroutine: Arccosine"
+                },
+                "F496": {
+                    "name": "ATN",
+                    "comment": "System subroutine: Arctangent"
+                },
+                "F49A": {
+                    "name": "ASN",
+                    "comment": "System subroutine: Arcsine"
+                },
+                "F531": {
+                    "name": "DEG",
+                    "comment": "System subroutine: Convert degrees, minutes and seconds into decimal degrees"
+                },
+                "F564": {
+                    "name": "DMS",
+                    "comment": "System subroutine: Convert decimal degrees into degrees, minutes and seconds"
+                },
+                "F597": {
+                    "name": "ABS",
+                    "comment": "System subroutine: Absolute value"
+                },
+                "F59D": {
+                    "name": "SGN",
+                    "comment": "System subroutine: Return -1 if negative, 0 if zero and 1 if positive"
+                },
+                "F5BE": {
+                    "name": "INT",
+                    "comment": "System subroutine: Return largest whole number not greater than N"
+                },
+                "F89C": {
+                    "name": "POWER_RAISE",
+                    "comment": "System subroutine: Power raise"
+                },
+                "D0D2": {
+                    "name": "NUMERICAL_COMPARISON",
+                    "comment": "System subroutine: Comparison of two numeric values"
+                },
+                "D0F9": {
+                    "name": "CHARACTER_STRING_COMPARISON",
+                    "comment": "System subroutine: Comparison of two strings"
+                },
+                "D2EA": {
+                    "name": "LINE_NUMBER_SEARCH",
+                    "comment": "System subroutine: Search for a BASIC line number"
+                },
+                "E42C": {
+                    "name": "KEY_SCAN_I",
+                    "comment": "System subroutine: Return the keycode of the key that was depressed"
+                },
+                "D461": {
+                    "name": "VARIABLE_SEARCH",
+                    "comment": "System subroutine: Search for a BASIC variable"
+                },
+                "E243": {
+                    "name": "KEY_SCAN_II",
+                    "comment": "System subroutine: Wit for a key to be depressed"
+                },
+                "E33F": {
+                    "name": "AUTO_POWER_OFF",
+                    "comment": "System subroutine: Configure auto-power off"
+                },
+                "E8CA": {
+                    "name": "PROGRAM_DISPLAY",
+                    "comment": "System subroutine: Display a BASIC line number"
+                },
+                "EDEF": {
+                    "name": "GRAPHIC_DISPLAY",
+                    "comment": "System subroutine: Display a grpahic"
+                },
+                "ED95": {
+                    "name": "HEXADECIMAL",
+                    "comment": "System subroutine: Convert an hexadecimal string into a numeric value"
+                },
+                "ED57": {
+                    "name": "ONE_CHARACTER_DISPLAY",
+                    "comment": "System subroutine: Display one character at cursor position"
+                },
+                "ED3B": {
+                    "name": "N_CHARACTER_DISPLAY'",
+                    "comment": "System subroutine: Display N characters starting from cursor position"
+                },
+                "ED4D": {
+                    "name": "CURSOR_MOVE_AFTER_ON_CHARACTER_DISPLAY",
+                    "comment": "System subroutine: Display one character and update cursor position"
+                },
+                "ED00": {
+                    "name": "CURSOR_MOVE_AFTER_N_CHARACTER_DISPLAY",
+                    "comment": "System subroutine: Display N characters and update cursor position"
+                },
+                "E000": {
+                    "name": "RESET_SUBROUTINE",
+                    "comment": "Reset Subroutine"
+                },
+                "E004": {
+                    "comment": "Disable timer"
+                },
+                "E006": {
+                    "comment": "LCD off"
+                },
+                "E00A": {
+                    "comment": "Wait for 192*11 CPU cycles (about 1.6 ms)"
+                },
+                "E00C": {
+                    "comment": "LCD on"
+                },
+                "E010": {
+                    "comment": "Wait for 1.6 ms"
+                },
+                "E012": {
+                    "comment": "LCD off"
+                },
+                "E014-E01C": {
+                    "comment": "Wait for 11,496 CPU cycles (about 9 ms)"
+                }
+            }
+        }
     }
 }
 ''';
