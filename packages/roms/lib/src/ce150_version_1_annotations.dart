19b8afec9c6a40cb2b4d2830e198fcacf4828a02
Christophe Bonello <cbonello@users.noreply.github.com>
2020-08-20 08:10:09 -0400

    add CE-150 ROM annotations

--- packages/roms/lib/src/ce150_version_1_annotations.dart
+++ packages/roms/lib/src/ce150_version_1_annotations.dart
@@ -2,6 +2,57 @@ const String json = '''
 {
     "A800": {
         "comment": "CE-150 ROM version: 44H = version 0, BEH = version 1"
+    },
+    "A519": {
+        "comment": "Printer: Color designation"
+    },
+    "A781": {
+        "comment": "Printer: Print"
+    },
+    "A9F1": {
+        "comment": "Printer: Linefeed"
+    },
+    "AA04": {
+        "comment": "Printer: Paper feed"
+    },
+    "ACBB": {
+        "comment": "Printer: Get TEXT mode ready"
+    },
+    "AAE3": {
+        "comment": "Printer: Pen up/down"
+    },
+    "A8DD": {
+        "comment": "Printer: Motor drive"
+    },
+    "A769": {
+        "comment": "Printer: Motor off"
+    },
+    "ABEF": {
+        "comment": "Printer: Get GRAPHIC mode ready"
+    },
+    "BF11": {
+        "comment": "Cassette tape: Remote on"
+    },
+    "BF43": {
+        "comment": "Cassette tape: Remote off"
+    },
+    "BDCC": {
+        "comment": "Cassette tape: One character save"
+    },
+    "BDF0": {
+        "comment": "Cassette tape: One character load"
+    },
+    "BCEB": {
+        "comment": "Cassette tape: Header input/output"
+    },
+    "BBF5": {
+        "comment": "Cassette tape: CMT I/O control"
+    },
+    "BBD6": {
+        "comment": "Cassette tape: Create header"
+    },
+    "BD3C": {
+        "comment": "Cassette tape: Transfer file"
     }
 }
 ''';
