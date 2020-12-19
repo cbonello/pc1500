4f01167854543cc87d6d9cf20fef0a188b233be3
Christophe Bonello <cbonello@users.noreply.github.com>
2020-12-19 17:19:48 -0500

    add messages to/from emulator

---
+++ packages/device/lib/src/messages/message.dart
@@ -0,0 +1,17 @@
+import 'dart:typed_data';
+
+import 'package:meta/meta.dart';
+
+enum MessageId { setDeviceType, setDebutPort, lcdEvent }
+
+abstract class Message {
+  Message({@required this.messageId}) : assert(messageId != null);
+
+  final MessageId messageId;
+}
+
+abstract class MessageSerializer<T> {
+  Uint8List serialize(T _) => throw UnimplementedError();
+
+  T deserialize(Uint8List _) => throw UnimplementedError();
+}
