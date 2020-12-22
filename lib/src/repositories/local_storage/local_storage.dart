a5434f44a282aba23e94425f759c285eea3a4cd4
Christophe Bonello <cbonello@users.noreply.github.com>
2020-12-21 21:16:30 -0500

    export debug_port_repository.dart

--- lib/src/repositories/local_storage/local_storage.dart
+++ lib/src/repositories/local_storage/local_storage.dart
@@ -1,2 +1,3 @@
-export './device_type_repository.dart';
-export './local_storage_repository.dart';
+export 'debug_port_repository.dart';
+export 'device_type_repository.dart';
+export 'local_storage_repository.dart';
