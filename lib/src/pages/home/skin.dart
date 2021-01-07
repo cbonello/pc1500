00da2edbf6b7360b8b931f75a715b7060b9db043
Christophe Bonello <cbonello@users.noreply.github.com>
2021-01-06 19:50:08 -0500

    add RawKeyboardListener widget

--- lib/src/pages/home/skin.dart
+++ lib/src/pages/home/skin.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:fluttericon/font_awesome_icons.dart';
 import 'package:fluttericon/mfg_labs_icons.dart';
 import 'package:fluttericon/modern_pictograms_icons.dart';
@@ -7,7 +8,7 @@ import 'package:google_fonts/google_fonts.dart';
 import '../../repositories/systems/models/models.dart';
 import 'lcd.dart';
 
-class Skin extends StatelessWidget {
+class Skin extends StatefulWidget {
   const Skin({Key key, @required this.skin, @required this.lcd})
       : assert(skin != null),
         assert(lcd != null),
@@ -17,45 +18,149 @@ class Skin extends StatelessWidget {
   final LcdWidget lcd;
 
   @override
+  _SkinState createState() => _SkinState();
+}
+
+class _SkinState extends State<Skin> {
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        lcd,
-        Image.asset(skin.image),
-        ...skin.keys.values.map<Widget>(
-          (KeyModel key) {
-            return Positioned(
-              left: key.left,
-              top: key.top,
-              child: Container(
-                height: key.height,
-                width: key.width,
-                decoration: BoxDecoration(
-                  color: Color(skin.keyColors[key.color].background),
-                  border: Border(
-                    right: BorderSide(
-                      color: Color(skin.keyColors[key.color].border),
-                      width: 0.5,
-                    ),
-                    bottom: BorderSide(
-                      color: Color(skin.keyColors[key.color].border),
-                      width: 0.5,
+    final Map<LogicalKeyboardKey, Widget> btnsMap =
+        <LogicalKeyboardKey, Widget>{};
+
+    return RawKeyboardListener(
+      focusNode: _focusNode,
+      autofocus: true,
+      onKey: (RawKeyEvent event) async {
+        if (event.runtimeType.toString() == 'RawKeyUpEvent') {
+          print(event.logicalKey);
+        }
+      },
+      child: Stack(
+        children: <Widget>[
+          widget.lcd,
+          Image.asset(widget.skin.image),
+          ...widget.skin.keys.values.map<Widget>(
+            (KeyModel key) {
+              return Positioned(
+                left: key.left,
+                top: key.top,
+                child: Container(
+                  height: key.height,
+                  width: key.width,
+                  decoration: BoxDecoration(
+                    color: Color(widget.skin.keyColors[key.color].background),
+                    border: Border(
+                      right: BorderSide(
+                        color: Color(widget.skin.keyColors[key.color].border),
+                        width: 0.5,
+                      ),
+                      bottom: BorderSide(
+                        color: Color(widget.skin.keyColors[key.color].border),
+                        width: 0.5,
+                      ),
                     ),
                   ),
+                  child: Builder(
+                    builder: (BuildContext context) {
+                      final Widget btn = _Button(
+                        label: key.label,
+                        color: Color(widget.skin.keyColors[key.color].color),
+                        fontSize: key.fontSize,
+                        onPressed: () {},
+                      );
+
+                      if (_keysToMap.containsKey(key.label)) {
+                        btnsMap[_keysToMap[key.label]] = btn;
+                      }
+
+                      return btn;
+                    },
+                  ),
                 ),
-                child: _Button(
-                  label: key.label,
-                  color: Color(skin.keyColors[key.color].color),
-                  fontSize: key.fontSize,
-                  onPressed: () {},
-                ),
-              ),
-            );
-          },
-        ).toList(),
-      ],
+              );
+            },
+          ).toList(),
+        ],
+      ),
     );
   }
+
+  static const Map<String, LogicalKeyboardKey> _keysToMap =
+      <String, LogicalKeyboardKey>{
+    'f1': LogicalKeyboardKey.f1,
+    'f2': LogicalKeyboardKey.f2,
+    'f3': LogicalKeyboardKey.f3,
+    'f4': LogicalKeyboardKey.f4,
+    'f5': LogicalKeyboardKey.f5,
+    'f6': LogicalKeyboardKey.f6,
+    'shift': LogicalKeyboardKey.shift,
+    'q': LogicalKeyboardKey.keyQ,
+    'w': LogicalKeyboardKey.keyW,
+    'e': LogicalKeyboardKey.keyE,
+    'r': LogicalKeyboardKey.keyR,
+    't': LogicalKeyboardKey.keyT,
+    'y': LogicalKeyboardKey.keyY,
+    'u': LogicalKeyboardKey.keyU,
+    'i': LogicalKeyboardKey.keyI,
+    'o': LogicalKeyboardKey.keyO,
+    'p': LogicalKeyboardKey.keyP,
+    '7': LogicalKeyboardKey.digit7,
+    '8': LogicalKeyboardKey.digit8,
+    '9': LogicalKeyboardKey.digit9,
+    '/': LogicalKeyboardKey.slash,
+    'clear': LogicalKeyboardKey.delete,
+    'a': LogicalKeyboardKey.keyA,
+    's': LogicalKeyboardKey.keyS,
+    'd': LogicalKeyboardKey.keyD,
+    'f': LogicalKeyboardKey.keyF,
+    'g': LogicalKeyboardKey.keyG,
+    'h': LogicalKeyboardKey.keyH,
+    'j': LogicalKeyboardKey.keyJ,
+    'k': LogicalKeyboardKey.keyK,
+    'l': LogicalKeyboardKey.keyL,
+    '=': LogicalKeyboardKey.equal,
+    '4': LogicalKeyboardKey.digit4,
+    '5': LogicalKeyboardKey.digit5,
+    '6': LogicalKeyboardKey.digit6,
+    '*': LogicalKeyboardKey.numpadMultiply,
+    'z': LogicalKeyboardKey.keyZ,
+    'x': LogicalKeyboardKey.keyX,
+    'c': LogicalKeyboardKey.keyC,
+    'v': LogicalKeyboardKey.keyV,
+    'b': LogicalKeyboardKey.keyB,
+    'n': LogicalKeyboardKey.keyN,
+    'm': LogicalKeyboardKey.keyM,
+    '(': LogicalKeyboardKey.numpadParenLeft,
+    ')': LogicalKeyboardKey.numpadParenRight,
+    'up': LogicalKeyboardKey.arrowUp,
+    '1': LogicalKeyboardKey.digit1,
+    '2': LogicalKeyboardKey.digit2,
+    '3': LogicalKeyboardKey.digit3,
+    '-': LogicalKeyboardKey.numpadSubtract,
+    ' ': LogicalKeyboardKey.space,
+    'enter': LogicalKeyboardKey.enter,
+    'left': LogicalKeyboardKey.arrowLeft,
+    'right': LogicalKeyboardKey.arrowRight,
+    'down': LogicalKeyboardKey.arrowDown,
+    '0': LogicalKeyboardKey.digit0,
+    '.': LogicalKeyboardKey.period,
+    'ent': LogicalKeyboardKey.numpadEqual,
+    '+': LogicalKeyboardKey.numpadAdd,
+  };
 }
 
 class _Button extends StatelessWidget {
