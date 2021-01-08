2df33b22dd717639cee26be2adcb03cce1ad0656
Christophe Bonello <cbonello@users.noreply.github.com>
2021-01-07 19:18:03 -0500

    start to programmatic buttons push

--- lib/src/pages/home/skin.dart
+++ lib/src/pages/home/skin.dart
@@ -38,60 +38,38 @@ class _SkinState extends State<Skin> {
 
   @override
   Widget build(BuildContext context) {
-    final Map<LogicalKeyboardKey, Widget> btnsMap =
-        <LogicalKeyboardKey, Widget>{};
+    final Map<int, _Button> btnsMap = <int, _Button>{};
 
     return RawKeyboardListener(
       focusNode: _focusNode,
       autofocus: true,
-      onKey: (RawKeyEvent event) async {
+      onKey: (RawKeyEvent event) {
         if (event.runtimeType.toString() == 'RawKeyUpEvent') {
-          print(event.logicalKey);
+          if (btnsMap.containsKey(event.logicalKey.keyId)) {
+            // btnsMap[event.logicalKey.keyId].animate();
+          }
         }
       },
       child: Stack(
         children: <Widget>[
           widget.lcd,
           Image.asset(widget.skin.image),
-          ...widget.skin.keys.values.map<Widget>(
-            (KeyModel key) {
-              return Positioned(
-                left: key.left,
-                top: key.top,
-                child: Container(
-                  height: key.height,
-                  width: key.width,
-                  decoration: BoxDecoration(
-                    color: Color(widget.skin.keyColors[key.color].background),
-                    border: Border(
-                      right: BorderSide(
-                        color: Color(widget.skin.keyColors[key.color].border),
-                        width: 0.5,
-                      ),
-                      bottom: BorderSide(
-                        color: Color(widget.skin.keyColors[key.color].border),
-                        width: 0.5,
-                      ),
-                    ),
-                  ),
-                  child: Builder(
-                    builder: (BuildContext context) {
-                      final Widget btn = _Button(
-                        label: key.label,
-                        color: Color(widget.skin.keyColors[key.color].color),
-                        fontSize: key.fontSize,
-                        onPressed: () {},
-                      );
-
-                      if (_keysToMap.containsKey(key.label)) {
-                        btnsMap[_keysToMap[key.label]] = btn;
-                      }
-
-                      return btn;
-                    },
-                  ),
-                ),
+          ...widget.skin.keys.keys.map<Widget>(
+            (String value) {
+              final KeyModel key = widget.skin.keys[value];
+              final _Button btn = _Button(
+                value: value,
+                keyboardKey: key,
+                colors: widget.skin.keyColors[key.color],
+                fontSize: key.fontSize,
+                onPressed: () {},
               );
+
+              if (_keysToMap.containsKey(value)) {
+                btnsMap[_keysToMap[value]] = btn;
+              }
+
+              return Positioned(left: key.left, top: key.top, child: btn);
             },
           ).toList(),
         ],
@@ -99,99 +77,129 @@ class _SkinState extends State<Skin> {
     );
   }
 
-  static const Map<String, LogicalKeyboardKey> _keysToMap =
-      <String, LogicalKeyboardKey>{
-    'f1': LogicalKeyboardKey.f1,
-    'f2': LogicalKeyboardKey.f2,
-    'f3': LogicalKeyboardKey.f3,
-    'f4': LogicalKeyboardKey.f4,
-    'f5': LogicalKeyboardKey.f5,
-    'f6': LogicalKeyboardKey.f6,
-    'shift': LogicalKeyboardKey.shift,
-    'q': LogicalKeyboardKey.keyQ,
-    'w': LogicalKeyboardKey.keyW,
-    'e': LogicalKeyboardKey.keyE,
-    'r': LogicalKeyboardKey.keyR,
-    't': LogicalKeyboardKey.keyT,
-    'y': LogicalKeyboardKey.keyY,
-    'u': LogicalKeyboardKey.keyU,
-    'i': LogicalKeyboardKey.keyI,
-    'o': LogicalKeyboardKey.keyO,
-    'p': LogicalKeyboardKey.keyP,
-    '7': LogicalKeyboardKey.digit7,
-    '8': LogicalKeyboardKey.digit8,
-    '9': LogicalKeyboardKey.digit9,
-    '/': LogicalKeyboardKey.slash,
-    'clear': LogicalKeyboardKey.delete,
-    'a': LogicalKeyboardKey.keyA,
-    's': LogicalKeyboardKey.keyS,
-    'd': LogicalKeyboardKey.keyD,
-    'f': LogicalKeyboardKey.keyF,
-    'g': LogicalKeyboardKey.keyG,
-    'h': LogicalKeyboardKey.keyH,
-    'j': LogicalKeyboardKey.keyJ,
-    'k': LogicalKeyboardKey.keyK,
-    'l': LogicalKeyboardKey.keyL,
-    '=': LogicalKeyboardKey.equal,
-    '4': LogicalKeyboardKey.digit4,
-    '5': LogicalKeyboardKey.digit5,
-    '6': LogicalKeyboardKey.digit6,
-    '*': LogicalKeyboardKey.numpadMultiply,
-    'z': LogicalKeyboardKey.keyZ,
-    'x': LogicalKeyboardKey.keyX,
-    'c': LogicalKeyboardKey.keyC,
-    'v': LogicalKeyboardKey.keyV,
-    'b': LogicalKeyboardKey.keyB,
-    'n': LogicalKeyboardKey.keyN,
-    'm': LogicalKeyboardKey.keyM,
-    '(': LogicalKeyboardKey.numpadParenLeft,
-    ')': LogicalKeyboardKey.numpadParenRight,
-    'up': LogicalKeyboardKey.arrowUp,
-    '1': LogicalKeyboardKey.digit1,
-    '2': LogicalKeyboardKey.digit2,
-    '3': LogicalKeyboardKey.digit3,
-    '-': LogicalKeyboardKey.numpadSubtract,
-    ' ': LogicalKeyboardKey.space,
-    'enter': LogicalKeyboardKey.enter,
-    'left': LogicalKeyboardKey.arrowLeft,
-    'right': LogicalKeyboardKey.arrowRight,
-    'down': LogicalKeyboardKey.arrowDown,
-    '0': LogicalKeyboardKey.digit0,
-    '.': LogicalKeyboardKey.period,
-    'ent': LogicalKeyboardKey.numpadEqual,
-    '+': LogicalKeyboardKey.numpadAdd,
+  static final Map<String, int> _keysToMap = <String, int>{
+    'f1': LogicalKeyboardKey.f1.keyId,
+    'f2': LogicalKeyboardKey.f2.keyId,
+    'f3': LogicalKeyboardKey.f3.keyId,
+    'f4': LogicalKeyboardKey.f4.keyId,
+    'f5': LogicalKeyboardKey.f5.keyId,
+    'f6': LogicalKeyboardKey.f6.keyId,
+    // 'shift': LogicalKeyboardKey.shift.keyId,
+    'q': LogicalKeyboardKey.keyQ.keyId,
+    'w': LogicalKeyboardKey.keyW.keyId,
+    'e': LogicalKeyboardKey.keyE.keyId,
+    'r': LogicalKeyboardKey.keyR.keyId,
+    't': LogicalKeyboardKey.keyT.keyId,
+    'y': LogicalKeyboardKey.keyY.keyId,
+    'u': LogicalKeyboardKey.keyU.keyId,
+    'i': LogicalKeyboardKey.keyI.keyId,
+    'o': LogicalKeyboardKey.keyO.keyId,
+    'p': LogicalKeyboardKey.keyP.keyId,
+    '7': LogicalKeyboardKey.digit7.keyId,
+    '8': LogicalKeyboardKey.digit8.keyId,
+    '9': LogicalKeyboardKey.digit9.keyId,
+    '/': LogicalKeyboardKey.slash.keyId,
+    'clear': LogicalKeyboardKey.delete.keyId,
+    'a': LogicalKeyboardKey.keyA.keyId,
+    's': LogicalKeyboardKey.keyS.keyId,
+    'd': LogicalKeyboardKey.keyD.keyId,
+    'f': LogicalKeyboardKey.keyF.keyId,
+    'g': LogicalKeyboardKey.keyG.keyId,
+    'h': LogicalKeyboardKey.keyH.keyId,
+    'j': LogicalKeyboardKey.keyJ.keyId,
+    'k': LogicalKeyboardKey.keyK.keyId,
+    'l': LogicalKeyboardKey.keyL.keyId,
+    '=': LogicalKeyboardKey.equal.keyId,
+    '4': LogicalKeyboardKey.digit4.keyId,
+    '5': LogicalKeyboardKey.digit5.keyId,
+    '6': LogicalKeyboardKey.digit6.keyId,
+    '*': LogicalKeyboardKey.numpadMultiply.keyId,
+    'z': LogicalKeyboardKey.keyZ.keyId,
+    'x': LogicalKeyboardKey.keyX.keyId,
+    'c': LogicalKeyboardKey.keyC.keyId,
+    'v': LogicalKeyboardKey.keyV.keyId,
+    'b': LogicalKeyboardKey.keyB.keyId,
+    'n': LogicalKeyboardKey.keyN.keyId,
+    'm': LogicalKeyboardKey.keyM.keyId,
+    '(': LogicalKeyboardKey.numpadParenLeft.keyId,
+    ')': LogicalKeyboardKey.numpadParenRight.keyId,
+    'up': LogicalKeyboardKey.arrowUp.keyId,
+    '1': LogicalKeyboardKey.digit1.keyId,
+    '2': LogicalKeyboardKey.digit2.keyId,
+    '3': LogicalKeyboardKey.digit3.keyId,
+    '-': LogicalKeyboardKey.numpadSubtract.keyId,
+    ' ': LogicalKeyboardKey.space.keyId,
+    'enter': LogicalKeyboardKey.enter.keyId,
+    'left': LogicalKeyboardKey.arrowLeft.keyId,
+    'right': LogicalKeyboardKey.arrowRight.keyId,
+    'down': LogicalKeyboardKey.arrowDown.keyId,
+    '0': LogicalKeyboardKey.digit0.keyId,
+    '.': LogicalKeyboardKey.period.keyId,
+    'ent': LogicalKeyboardKey.numpadEqual.keyId,
+    // '+': LogicalKeyboardKey.numpadAdd.keyId,
   };
 }
 
-class _Button extends StatelessWidget {
+class _Button extends StatefulWidget {
   const _Button({
     Key key,
-    @required this.label,
-    @required this.color,
+    @required this.value,
+    @required this.keyboardKey,
+    @required this.colors,
     @required this.fontSize,
     @required this.onPressed,
-  })  : assert(label != null),
-        assert(color != null),
+  })  : assert(value != null),
+        assert(keyboardKey != null),
+        assert(colors != null),
         assert(fontSize != null),
         assert(onPressed != null),
         super(key: key);
 
-  final KeyLabelModel label;
-  final Color color;
+  final String value;
+  final KeyModel keyboardKey;
+  final ColorModel colors;
   final double fontSize;
   final VoidCallback onPressed;
 
   @override
+  __ButtonState createState() => __ButtonState();
+}
+
+class __ButtonState extends State<_Button> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  double width, height;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this);
+    width = widget.keyboardKey.width;
+    height = widget.keyboardKey.height;
+  }
+
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    super.dispose();
+  }
+
+  void animate() {
+    _animationController
+      ..forward()
+      ..reverse();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final Widget value = label.when<Widget>(
+    final Widget value = widget.keyboardKey.label.when<Widget>(
       text: (String value) {
         return Text(
           value,
           textAlign: TextAlign.center,
           style: GoogleFonts.openSans(
             textStyle: TextStyle(
-              color: color,
-              fontSize: fontSize,
+              color: Color(widget.colors.color),
+              fontSize: widget.fontSize,
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -202,30 +210,69 @@ class _Button extends StatelessWidget {
           case 'left':
             return Icon(
               ModernPictograms.left_dir,
-              size: fontSize,
-              color: color,
+              size: widget.fontSize,
+              color: Color(widget.colors.color),
             );
           case 'up':
-            return Icon(MfgLabs.up_bold, size: fontSize, color: color);
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Icon(
+                MfgLabs.up_bold,
+                size: widget.fontSize,
+                color: Color(widget.colors.color),
+              ),
+            );
           case 'right':
             return Icon(
               ModernPictograms.right_dir,
-              size: fontSize,
-              color: color,
+              size: widget.fontSize,
+              color: Color(widget.colors.color),
             );
           case 'down':
-            return Icon(MfgLabs.down_bold, size: fontSize, color: color);
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Icon(
+                MfgLabs.down_bold,
+                size: widget.fontSize,
+                color: Color(widget.colors.color),
+              ),
+            );
           case 'up-down':
-            return Icon(FontAwesome.sort, size: fontSize, color: color);
+            return Icon(
+              FontAwesome.sort,
+              size: widget.fontSize,
+              color: Color(widget.colors.color),
+            );
           default:
             throw Exception('Invalid icon type: $value');
         }
       },
     );
 
-    return TextButton(
-      onPressed: () {},
-      child: FittedBox(child: value),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 250),
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: Color(widget.colors.background),
+        border: Border(
+          right: BorderSide(
+            color: Color(widget.colors.border),
+            width: 0.5,
+          ),
+          bottom: BorderSide(
+            color: Color(widget.colors.border),
+            width: 0.5,
+          ),
+        ),
+      ),
+      child: GestureDetector(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: FittedBox(child: value),
+        ),
+      ),
     );
   }
 }
